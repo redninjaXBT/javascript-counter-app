c66313694fa2029d3b827fba203184ed4151f903
redninjaXBT <redninjaxbt@gmail.com>
2022-12-15 23:23:14 +0530

    added saved enteries section

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let count = 0;
   let currentCount = document.getElementById("current-count");
   function increaseCount() {
-    count = count + 1;
-    currentCount.innerText = count;
+    count++;
+    currentCount.textContent = count;
   }
   function resetCount() {
     count = 0;
     currentCount.innerText = count;
   }
   function decreaseCount() {
-    count = count - 1;
+    count--;
     currentCount.innerText = count;
+  }
+  
+  let savedEnteries = document.getElementById("saved-enteries");
+  function save() {
+    let saveCurrentEntry = count + ", ";
+    
+    savedEnteries.textContent += saveCurrentEntry;
   }
